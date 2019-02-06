da82c0a8f38a820818d6102267c57133c65fbd0a
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-06 08:19:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarf_array)# code an argument here
   # Your code here
-  dwarf_array.each_with_index do |n, i| puts "#{i}. #{n}" end
+  dwarf_array.each_with_index do |n, i| puts "#{i + 1}. #{n}" end
 end
 
 def summon_captain_planet# code an argument here
